29892d60be098a935f8669af0fdaf48d5cc26c10
AlonsoDRDLV <alonsodrdlv@gmail.com>
2023-04-17 20:19:58 +0200

    Update ejecutar.sh

--- p3/ejecutar.sh
+++ p3/ejecutar.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 g++ main.cpp -o main
-./main pruebas/diccionario_100.txt resultados/resultados_100.txt
-./main pruebas/diccionario_250.txt resultados/resultados_250.txt
-./main pruebas/diccionario_625.txt resultados/resultados_625.txt
-./main pruebas/diccionario_1562.txt resultados/resultados_1562.txt
-./main pruebas/diccionario_3906.txt resultados/resultados_3906.txt
+./main -i pruebas/diccionario_100.txt -o resultados/resultados_100.txt -l 100
+./main -i pruebas/diccionario_250.txt -o resultados/resultados_250.txt -l 100
+./main -i pruebas/diccionario_625.txt -o resultados/resultados_625.txt -l 100
+./main -i pruebas/diccionario_1562.txt -o resultados/resultados_1562.txt -l 100
+./main -i pruebas/diccionario_3906.txt -o resultados/resultados_3906.txt -l 100
+
+./main -i pruebas/diccionario_100.txt -o resultados/resultados_100.txt -p 10
+./main -i pruebas/diccionario_250.txt -o resultados/resultados_250.txt -p 10
+./main -i pruebas/diccionario_625.txt -o resultados/resultados_625.txt -p 10
+./main -i pruebas/diccionario_1562.txt -o resultados/resultados_1562.txt -p 10
+./main -i pruebas/diccionario_3906.txt -o resultados/resultados_3906.txt -p 10
+
+./main -i pruebas/diccionario_1562.txt -o resultados/resultados_1562.txt -p 2
+./main -i pruebas/diccionario_1562.txt -o resultados/resultados_1562.txt -p 20
